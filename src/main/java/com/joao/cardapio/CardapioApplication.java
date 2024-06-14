08fa97aaa3d97b592b8cd12c9951ba7ed3eb1307
JaoPedro007 <joaopedrorodriguesleite9@gmail.com>
2024-06-14 01:06:40 +0000

    End-point getAll of the table foods

---
+++ src/main/java/com/joao/cardapio/CardapioApplication.java
@@ -0,0 +1,15 @@
+package com.joao.cardapio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Scanner;
+
+@SpringBootApplication
+public class CardapioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardapioApplication.class);
+	}
+
+}
---
+++ src/main/java/com/joao/cardapio/controller/FoodController.java
@@ -0,0 +1,28 @@
+package com.joao.cardapio.controller;
+
+
+import com.joao.cardapio.food.Food;
+import com.joao.cardapio.food.FoodRepository;
+import com.joao.cardapio.food.FoodResponseDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("food")
+public class FoodController {
+    @Autowired
+    private FoodRepository repository;
+
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
+    @GetMapping
+    public List<FoodResponseDTO> getAll(){
+
+        List<FoodResponseDTO> foodList = repository.findAll().stream().map(FoodResponseDTO::new).collect(Collectors.toList());
+        return foodList;
+    }
+}
