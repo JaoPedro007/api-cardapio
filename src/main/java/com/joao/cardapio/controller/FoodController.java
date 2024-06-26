167bd849628f41c7d4bdf43ea7a1dad3629448cc
JaoPedro007 <joaopedrorodriguesleite9@gmail.com>
2024-06-26 00:50:34 +0000

    End-point saveFood of the table foods

--- src/main/java/com/joao/cardapio/controller/FoodController.java
+++ src/main/java/com/joao/cardapio/controller/FoodController.java
@@ -1,14 +1,11 @@
 package com.joao.cardapio.controller;
-
-
 import com.joao.cardapio.food.Food;
 import com.joao.cardapio.food.FoodRepository;
+import com.joao.cardapio.food.FoodRequestDTO;
 import com.joao.cardapio.food.FoodResponseDTO;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -18,10 +15,17 @@ public class FoodController {
     @Autowired
     private FoodRepository repository;
 
+
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
+    @PostMapping
+    public void saveFood(@RequestBody FoodRequestDTO data){
+        Food foods = new Food(data);
+        repository.save(foods);
+    }
+
     @CrossOrigin(origins = "*", allowedHeaders = "*")
     @GetMapping
     public List<FoodResponseDTO> getAll(){
-
         List<FoodResponseDTO> foodList = repository.findAll().stream().map(FoodResponseDTO::new).collect(Collectors.toList());
         return foodList;
     }
--- src/main/java/com/joao/cardapio/food/Food.java
+++ src/main/java/com/joao/cardapio/food/Food.java
@@ -14,12 +14,16 @@ import lombok.NoArgsConstructor;
 @EqualsAndHashCode(of = "id")
 public class Food {
 
-    @Id @GeneratedValue (strategy = GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
     private long id;
-
     private String title;
-
     private String image;
-
     private Integer price;
+
+    public Food(FoodRequestDTO data){
+        this.title = data.title();
+        this.image = data.image();
+        this.price = data.price();
+    }
 }
